dd39a1a1faa11d5b96c81563718c40bf1a8d6f70
HuangChaoYi <1089109@qq.com>
2020-06-19 17:10:34 +0800

    AM-10 订单列表-测试1

--- src/services/orderList.ts
+++ src/services/orderList.ts
@@ -3,7 +3,7 @@
  * @Email: 1089109@qq.com
  * @Date: 2020-05-29 10:29:40
  * @LastEditors: Huang Chao Yi
- * @LastEditTime: 2020-06-19 15:20:59
+ * @LastEditTime: 2020-06-19 17:09:44
  * @FilePath: \amzics-react\src\services\orderList.ts
  * 订单列表API
  */ 
@@ -13,7 +13,7 @@ export async function getOrderLists(params: {}) {
   return request(`/api/mws/order/list`, {
     params,
     headers: {
-      StoreId: '1261140381664944130',
+      StoreId: '1261140381664944131',
     },
   });
 }
