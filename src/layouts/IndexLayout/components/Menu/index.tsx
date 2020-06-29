c59fd203fdbbc6279c9ee1e81327e83d5fceb262
ranyang <931892155@qq.com>
2020-06-29 20:58:19 +0800

    AM-40 增加从其他页面到index的头部连接

--- src/layouts/IndexLayout/components/Menu/index.tsx
+++ src/layouts/IndexLayout/components/Menu/index.tsx
@@ -8,12 +8,18 @@ interface IMenuItemProps {
 }
 
 const { Link } = Anchor;
+console.log('location:', location);
 
-const menuList: Array<IMenuItemProps> = [
+const menuList: Array<IMenuItemProps> = location.pathname === '/index/' ? [
   { href: '#home', name: '首页', top: 0 },
   { href: '#fun', name: '功能', top: 864 },
   { href: '#pay', name: '付费', top: 1644 },
   { href: '#fqa', name: 'FAQ', top: 2359 },
+] : [
+  { href: '/index/#home', name: '首页', top: 0 },
+  { href: '/index/#fun', name: '功能', top: 864 },
+  { href: '/index/#pay', name: '付费', top: 1644 },
+  { href: '/index/#fqa', name: 'FAQ', top: 2359 },
 ];
 
 const MenuCom: React.FC = () => {
