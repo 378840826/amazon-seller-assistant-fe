066b23201a5fc0b39ea0529df86825dbf0b8c2b5
ranyang <931892155@qq.com>
2021-02-04 18:25:46 +0800

    AM-137 竞品上下箭修改

--- src/components/UpAndDown/index.tsx
+++ src/components/UpAndDown/index.tsx
@@ -3,34 +3,30 @@
  * @param {value} 显示的值 number类型，每三位加一个逗号
  * @param {isAdd} 默认false,带箭头的显示方式 为true为带有加减号的形式
  * @param {inline} 默认false最外层的div display:block,为true时显示为inline-block
- * @param {rise} 为不用靠数字正负判断上下箭头还是加减号，rise为空，那么不显示标记,默认为true
- * 
  */
 import React from 'react';
 import classnames from 'classnames';
 import { Iconfont, toThousands } from '@/utils/utils';
 import styles from './index.less';
 
-
 interface IUpAndDown{
     className?: string;
     value: number | string;
     isAdd?: boolean;
     inline?: boolean;
-    rise?: boolean | string;
 }
-const RenderArrow = ({ value, rise }: IUpAndDown) => {
+const RenderArrow = ({ value }: IUpAndDown) => {
   return (
     <>
       { value > 0 ? 
         <>
           <span className={styles.__font}>{toThousands(value)}</span>
-          {rise === '' ? '' : <Iconfont className={styles.green} type="icon-xiajiang"/> }
+          <Iconfont className={styles.green} type="icon-xiajiang"/> 
         </>
         :
         <>
           <span>{toThousands(Math.abs(value as number))}</span>
-          {rise === '' ? '' : <Iconfont className={styles.red} type="icon-xiajiang"/> } 
+          <Iconfont className={styles.red} type="icon-xiajiang"/>
         </>
       }
     </>
@@ -43,7 +39,6 @@ const UpAndDown: React.FC<IUpAndDown> = ({
   value, 
   isAdd = false,
   inline = false,
-  rise = true,
 }) => {
   if (value === '') {
     return <div className="null_bar"></div>; 
@@ -65,7 +60,7 @@ const UpAndDown: React.FC<IUpAndDown> = ({
               <span className={styles.red}>-{toThousands(Math.abs(value as number))}</span>
             }
           
-          </> : <RenderArrow rise={rise} value={value}/>
+          </> : <RenderArrow value={value}/>
       }
     </div>
   );
